36355cc1bfa4278fb9dadd87b6019b4330e741f6
Iremos <biopet@snu.ac.kr>
2026-07-14 17:49:33 +0900

    기존 제작한 최근 게시글 및 지도 둘러보기 기능 메인 페이지에 병합

--- LocalHub/src/App.vue
+++ LocalHub/src/App.vue
@@ -1,8 +1,35 @@
 <script setup>
 import NavigationBar from './components/Navigationbar.vue'
+import RecentPosts from './components/recent_posts.vue'
+import MapCheck from './components/map_check.vue'
 </script>
 
 <template>
   <NavigationBar />
   <router-view />
+  <div class="widgets" v-if="$route.meta.showWidget">
+    <RecentPosts />
+    <MapCheck />
+  </div>
 </template>
+
+<style scoped>
+  .widgets {
+  display: flex;
+  gap: 20px;
+  margin: 20px;
+}
+
+.widgets > * {
+  flex: 1 1 50%;
+  min-width: 0;
+}
+
+/* 모바일에서 세로 스택 */
+@media (max-width: 768px) {
+  .widgets {
+    flex-direction: column;
+  }
+}
+
+</style>
--- LocalHub/src/router/index.js
+++ LocalHub/src/router/index.js
@@ -5,10 +5,10 @@ import PostView from '../views/PostView.vue'
 import BoardEditor from '../views/BoardEditor.vue'
 
 const routes = [
-  { path: '/', name: 'home', component: Home },
-  { path: '/board/:category', name: 'board', component: BoardPage, props: true },
-  { path: '/post/:id', name: 'post', component: PostView, props: true },
-  { path: '/new', name: 'new', component: BoardEditor }
+  { path: '/', name: 'home', component: Home, meta: { showWidget: true } },
+  { path: '/board/:category', name: 'board', component: BoardPage, props: true, meta: { showWidget: true } },
+  { path: '/post/:id', name: 'post', component: PostView, props: true, meta: { showWidget: true } },
+  { path: '/new', name: 'new', component: BoardEditor, meta: { showWidget: true } }
 ]
 
 export default createRouter({
